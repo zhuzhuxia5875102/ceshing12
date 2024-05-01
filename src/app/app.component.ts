2c59ad62fca06f250007cb059087fdcbb8f3d98a
huajian123 <287643967@qq.com>
2024-05-01 19:44:34 +0800

    为添加mf的版本

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  template: `
+    <div class="mydiy">app组件mydiy</div>
+    <button nz-button nzType="primary">app组件button，此时没有zorro</button>
+    <app-me></app-me>
+  `,
 })
 export class AppComponent {
-  title = 'ceshing12';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeComponent } from './me/me.component';
+import { ChildrenComponent } from './me/children/children.component';
+import { NzButtonModule} from "ng-zorro-antd/button";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeComponent,
+    ChildrenComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    NzButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-  constructor() {
-  }
-}
+export class AppModule { }
