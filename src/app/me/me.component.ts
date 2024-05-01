1159b87a03eb2afae70ae08081f16423e248b2d9
huajian123 <287643967@qq.com>
2024-05-01 19:44:30 +0800

    为添加mf的版本

---
+++ src/app/me/me.component.ts
@@ -0,0 +1,11 @@
+import {Component, ViewEncapsulation} from '@angular/core';
+
+@Component({
+  selector: 'app-me',
+  templateUrl: './me.component.html',
+  styleUrls: ['./me.component.less'],
+  encapsulation: ViewEncapsulation.ShadowDom
+})
+export class MeComponent {
+
+}
